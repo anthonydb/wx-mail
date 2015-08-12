94746d7e0283ccc0927672185788fec456768882
anthonydb <adebarros@gmail.com>
2015-08-12 14:45:47 -0400

    Now with functions.

--- dailymail.py
+++ dailymail.py
@@ -11,49 +11,55 @@ def fetch_forecast(api_key):
     j = json.loads(r.text)
     return j
 
-json = fetch_forecast(api_key)
+def build_html(json):
+    # build some HTML snippets to open and close this email
+    html_open = """\
+    <html>
+      <head></head>
+      <body>
+    """
+    html_close = """\
+      </body>
+    </html>
+    """
 
+    # let's now build the HTML body contents
+    wxdate = json['forecast']['txt_forecast']['date']
+    mail_text = '<p><b>Hello, DeBarros family!</b></p><p>Here is the Leesburg, Va., weather forecast as of ' + wxdate + '</p>'
+    forecast_length = len(json['forecast']['txt_forecast']['forecastday']) - 1
 
-# Set the current time and add that to the message subject
-cur_date = datetime.date.today().strftime("%B") +\
-    ' ' + datetime.date.today().strftime("%d") +\
-    ', ' + datetime.date.today().strftime("%Y")
-subject = 'Family forecast for ' + cur_date
+    # looping through the JSON object
+    for i in range(0, forecast_length):
+        cast = '<p><b>' + json['forecast']['txt_forecast']['forecastday'][i]['title'] + '</b>: ' +\
+               json['forecast']['txt_forecast']['forecastday'][i]['fcttext'] + '</p>'
+        mail_text += cast
 
+    # put it all together
+    html_body = html_open + mail_text + html_close
+    return html_body
 
-html_open = """\
-<html>
-  <head></head>
-  <body>
-"""
+def send_email(mail_text):
+    # Set the current time and add that to the message subject
+    cur_date = datetime.date.today().strftime("%B") +\
+        ' ' + datetime.date.today().strftime("%d") +\
+        ', ' + datetime.date.today().strftime("%Y")
+    subject = 'Family forecast for ' + cur_date
 
-html_close = """\
-  </body>
-</html>
-"""
+    # Set up the message subject, etc. Then send it.
+    COMMASPACE = ', '
 
-wxdate = json['forecast']['txt_forecast']['date']
+    msg = MIMEText(mail_text, 'html')
+    msg['Subject'] = subject
+    msg['From'] = 'DeBarros Family Hackr Bot'
+    msg['To'] = COMMASPACE.join(outgoing_mails)
 
-mail_text = '<p><b>Hello, DeBarros family!</b></p><p>Here is the Leesburg, Va., weather forecast as of ' + wxdate + '</p>'
-forecast_length = len(json['forecast']['txt_forecast']['forecastday']) - 1
+    server = smtplib.SMTP(mail_settings['smtp'], 25)
+    server.login(mail_settings['address'], mail_settings['pw'])
+    server.set_debuglevel(1)
+    server.sendmail(mail_settings['address'], outgoing_mails, msg.as_string())
+    server.quit()
 
-for i in range(0, forecast_length):
-    cast = '<p><b>' + json['forecast']['txt_forecast']['forecastday'][i]['title'] + '</b>: ' +\
-           json['forecast']['txt_forecast']['forecastday'][i]['fcttext'] + '</p>'
-    mail_text += cast
-
-mail_text = html_open + mail_text + html_close
-
-# mail stuff
-COMMASPACE = ', '
-
-msg = MIMEText(mail_text, 'html')
-msg['Subject'] = subject
-msg['From'] = 'DeBarros Family Hackr Bot'
-msg['To'] = COMMASPACE.join(outgoing_mails)
-
-server = smtplib.SMTP(mail_settings['smtp'], 25)
-server.login(mail_settings['address'], mail_settings['pw'])
-server.set_debuglevel(1)
-server.sendmail(mail_settings['address'], outgoing_mails, msg.as_string())
-server.quit()
+if __name__ == "__main__":
+    json = fetch_forecast(api_key)
+    mail_text = build_html(json)
+    send_email(mail_text)
